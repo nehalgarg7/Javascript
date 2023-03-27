3ea304303ef0da633bcc81ebc28061a601727215
Nehal <nehalgarg37@gmail.com>
2023-03-27 14:56:02 +0530

    commit

---
+++ Function/AnonymousFunction.js
@@ -0,0 +1,9 @@
+// These function don't have any name but can be assigned to a variable.
+// Arrow functions are also anonymous function with a compact syntax.
+
+let x = function ()
+{
+    console.log("hi");
+}
+
+x();
---
+++ Object/ObjectMethods.js
@@ -0,0 +1,27 @@
+let emp = {
+    id : 101,
+    name: 'Sanket',
+    age: 24
+}
+
+let keys = Object.keys(emp); // Extract all keys of the object
+console.log(keys);
+
+let values = Object.values(emp); // Extract all the values of the object
+console.log(keys);
+
+let entries = Object.entries(emp); // Extract key -  values pair of the objects
+console.log(entries);
+
+Object.freeze(emp); // stop add, delete and update of an object.
+emp.address = "Naa";
+emp.id = 100;
+console.log(emp);
+
+Object.seal(emp); // allow update but can't add or delete anything
+emp.address = "Naa";
+emp.id = 100;
+console.log(emp); 
+
+let o = Object.assign({x:16}, emp); // assign all the key-value pair to new object o and also add new key-value to it by passing it under the {}.
+console.log(o);
