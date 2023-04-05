dc953646e70fda97c04386a73f8400cad5da2eb8
Nehal <nehalgarg37@gmail.com>
2023-04-05 15:45:12 +0530

    Error Handling

---
+++ Error Handling and Prototypes/TryAndCatch.js
@@ -0,0 +1,22 @@
+/**
+ * Syntax of try-catch
+ * try{
+ * //block of code for testing}
+ * catch{
+ * //block of code for addressing errors.
+ * }finally{
+ * //Block of code to run after try-catch}
+ */
+// let x = undefined;
+// console.log(x[0]);
+
+try {
+    let x = undefined;
+    console.log(x[0]);
+}
+catch (err) {
+    console.log("Catch blocks get executed",err);
+}
+finally {
+    console.log("Finally always gets executed");
+}
